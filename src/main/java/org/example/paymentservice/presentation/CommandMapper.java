d7fc3fb5037c3510d95c1f6e2cba205efcd248c6
mvjq <mviniciusjunqueira@gmail.com>
2025-10-31 13:44:11 -0300

    Add CommandMapper and PaymentServiceApplication classes

---
+++ src/main/java/org/example/paymentservice/presentation/CommandMapper.java
@@ -0,0 +1,31 @@
+package org.example.paymentservice.presentation;
+
+import jakarta.validation.Valid;
+import org.example.paymentservice.application.command.PaymentCommand;
+import org.example.paymentservice.application.command.WebhookCommand;
+import org.example.paymentservice.domain.model.Webhook;
+import org.example.paymentservice.presentation.dto.PaymentRequest;
+import org.example.paymentservice.presentation.dto.WebhookRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandMapper {
+
+    public PaymentCommand toCommand(PaymentRequest request) {
+        return PaymentCommand.builder()
+                    .firstName(request.firstName())
+                    .lastName(request.lastName())
+                    .zipCode(request.zipCode())
+                    .cardNumber(request.cardNumber())
+                    .build();
+    }
+
+    public WebhookCommand toCommand(WebhookRequest request) {
+        return WebhookCommand.builder()
+                .url(request.getUrl())
+                .secret(request.getSecret())
+                .description(request.getDescription())
+                .build();
+    }
+
+}
